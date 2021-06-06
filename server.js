aa7f49e49310b183c562c20b9d7bc4a16ccee070
Kamlesh Sarki <kamleshsarki@Kamleshs-MacBook-Pro.local>
2021-06-06 14:05:06 -0700

    route updated

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
 const express = require('express')
 const path = require('path')
+const mysql = require('mysql2')
+
 
 const app = express()
-let songs=[
-    {
-        title: 'Thunderstruck',
-        artist: 'ACDC'
-    },
-    {
-        title: 'Dancing Queen',
-        artist: 'ABBA'
-    }
-]
+const db = mysql.createConnection({
+ host: 'localhost',
+ user: 'root',
+ password: 'Password',
+ database: 'fsongs_db'
+})
+
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 //Get all songs
-app.get('/songs', (req, res)=>{
-    res.json(songs)
-
+app.get('/fsongs', (req, res)=>{
+    db.query('SELECT * FROM fsongs',(e, fsongs)=>{
+     if(e){
+       console.log(e)
+     }
+        res.json(fsongs)
+    })  
 })
 
 // Post a song
-app.post('/songs',(req,res)=>{
-//req body
-    songs.push(req.body)
-    res.sendStatus(200)
+app.post('/fsongs',(req,res)=>{
+ db.query(`INSERT INTO fsongs (title, artist) VALUES("${req.body.title} ",
+  " ${req.body.artist}")`,e => {
+      if(e){
+          console.log(e)
+      }
+      res.sendStatus(200)
+  })   
 })
-
 
 //Delete a song
-app.delete('/songs/:title', (req, res)=>{
-    //req.params.title
-    songs = songs.filter(song=>song.title != req.params.title)
-    res.sendStatus(200)
+app.delete('/fsongs/:title', (req, res)=>{
+    db.query(`DELETE FROM fsongs WHERE title = "${req.params.title}"`, e=>{
+        if(e){
+            console.log(e)
+        }
+        res.sendStatus(200)
+    })
 })
-
 app.listen(3000)
